cefaf5314c2c72baa1ddcffe38155dda3ee0f6ea
keniarios <judith23_rios@hotmail.com>
2018-11-09 11:08:17 -0700

    fgdfhs

--- mostrarCiudadyTiendas/conexion.php
+++ mostrarCiudadyTiendas/conexion.php
@@ -2,13 +2,13 @@
 
 function conectarBDCS()
 {
-	$host = "host=localhost";
-	$port = "port=5432";
-	$dbname = "dbname=CINEPOLIS";
-	$user = "user=postgres";
-	$password = "password=123";
+	
+	$host = "host=equipo4.postgres.database.azure.com";
+	$dbname = "dbname=cinepolis";
+	$user = "user=quipo4@equipo4";
+	$password = "password=Judith123";
 
-	$bd = pg_connect("$host $port $dbname $user $password");
+	$bd = pg_connect("$host $dbname $user $password");
 	if (!$bd ) {
 		echo "Error: " .pg_last_error();
 	}else{
