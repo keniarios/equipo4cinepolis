3597475ee73b0c2fde9d8bc61c6317c4ba2f5139
keniarios <judith23_rios@hotmail.com>
2018-11-11 21:58:53 -0700

    gdhytui
    ouo

--- bd/conexion.php
+++ bd/conexion.php
@@ -7,7 +7,7 @@ function conectarBD()
 	$user = "user=equipo4@equipo4";
 	$password = "password=Judith123";
 
-	$bd = pg_connect("$host $dbname $user");
+	$bd = pg_connect("$host $dbname $user $password");
 	if (!$bd ) {
 		echo "Error: " .pg_last_error();
 	}else{
