92775ae6e17469172bdc796f823fe0c85073de66
keniarios <judith23_rios@hotmail.com>
2018-11-12 01:07:16 -0700

    asdnknfcgd

--- verSinopsis/conexion.php
+++ verSinopsis/conexion.php
@@ -2,23 +2,16 @@
 
 function conectarBDverSinosis()
 {
-	$host = "host=localhost";
-	$port = "port=5432";
-	$dbname = "dbname=CINEPOLIS";
-	$user = "user=postgres";
-	$password = "password=123";
+	$host = "host=equipo4.postgres.database.azure.com";
+	$dbname = "dbname=cinepolis";
+	$user = "user=equipo4@equipo4";
+	$password = "password=Judith123";
 
-	$bd = pg_connect("$host $port $dbname $user $password");
+	$bd = pg_connect("$host $dbname $user $password");
 	if (!$bd ) {
 		echo "Error: " .pg_last_error();
 	}else{
-		//echo "<H3>Conexion Exitosa - PosgreSQL<H3><HR>";
-
 		return $bd;
 	}
 }
- ?>
-
-
-
-
+?>
