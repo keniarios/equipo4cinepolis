bb928c77c472ceac392c9a96a0af38aafb2ddd2a
keniarios <judith23_rios@hotmail.com>
2018-11-12 01:25:32 -0700

    sadsd

--- vistas/header/encabezado2.php
+++ vistas/header/encabezado2.php
@@ -5,15 +5,15 @@
 <head>
 	<title></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
-	<script src='../js/jquery-3.3.1.min.js'></script>
-	<script src='../js/popper.min.js'></script>
-	<script src='../js/bootstrap.min.js'></script>
+	<script src='../../js/jquery-3.3.1.min.js'></script>
+	<script src='../../js/popper.min.js'></script>
+	<script src='../../js/bootstrap.min.js'></script>
 	<link rel='stylesheet' href='../css/bootstrap.min.css'>
 	<link rel='stylesheet' type='text/css' href='../css/estilo.css'>
 	
-	<script src="../js/misfunciones.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="../../js/misfunciones.js" type="text/javascript" charset="utf-8" async defer></script>
 	<link rel="stylesheet" href="../css/jquery-ui.css">
-  	<script src="../js/jquery-ui.js"></script>
+  	<script src="../../js/jquery-ui.js"></script>
 
 </head>
 <body>
