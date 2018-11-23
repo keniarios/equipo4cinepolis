0a6554342aa793ed69e22762e51a29f025571b4c
keniarios <judith23_rios@hotmail.com>
2018-11-23 11:17:40 -0700

    dasdas

--- cinepolisID/mostrarCiudadyTiendas/mostrarCiudadyTiendas.php
+++ cinepolisID/mostrarCiudadyTiendas/mostrarCiudadyTiendas.php
@@ -5,10 +5,20 @@
 
 	include_once ('conexion.php'); $conexion = conectarBDCIDCS();
 
+    $ParaMetrogetsucursal="";
+	if (isset($_GET['id_sucursal'])) {
+		$ParaMetrogetsucursal=$_GET['id_sucursal'];
+		$resultSucursalNombre = pg_query("SELECT nombre FROM altasucursal WHERE estatus=1 and id_sucursal='$ParaMetrogetsucursal'");
+		
+		while ($datoSucursal=pg_fetch_array($resultSucursalNombre)) {
+			$NombreSucursal= $datoSucursal['nombre'];
+		}
+	}
     $resultSucursales =  pg_query("SELECT DISTINCT ON (ciudad) ciudad, id_sucursal, nombre FROM altasucursal WHERE estatus=1  ORDER BY ciudad");
-    $resultSucursales2 = pg_query("SELECT ciudad, id_sucursal, nombre FROM altasucursal WHERE estatus=1 and ciudad='Culiacán' ORDER BY ciudad");
-	
     pg_close($conexion);
+
+
+    $ParaMetrogetciudad="";
 ?>
 <!DOCTYPE html>
 <html>
@@ -24,21 +34,39 @@
 			<div class="selectBlanco" style="background: #fff; border: solid 0px #e1e3e6; border-radius: 3px;">
 				<div class="selectorr" id="selectorrciudad" style="width: 294px; background: url('../img/vistaCliente/icon-select-form-gris.png?embed') no-repeat 100% 0; -webkit-text-overflow: ellipsis;-moz-text-overflow: ellipsis;-ms-text-overflow: ellipsis; -o-text-overflow: ellipsis; text-overflow: ellipsis; display: block; overflow: hidden; white-space: nowrap; background-position: right 0; height: 35px; line-height: 35px; padding-right: 30px; cursor: pointer; width: 99.9% !important; display: block; color: #868788;">
 					<span style="width: 282px;">
-					<select name="opcionCiudades" id="opcionCiudades">
-						<option value="Selecciona una ciudad">Selecciona una ciudad</option>
+					<?PHP $x=0; ?>
 						<?php 
-							$bandera=0;
-							$banderaS = false;
+						if (isset($_GET['ciudad'])) {
+							$ParaMetrogetciudad=$_GET['ciudad'];
+						echo "<select name='opcionCiudades' id='opcionCiudades'>
+							<option value='$ParaMetrogetciudad'>$ParaMetrogetciudad</option>";
+
 							while ($datos=pg_fetch_array($resultSucursales)) {
 								$ciudad = $datos['ciudad'];
-								if ($ciudad == "Culiacán" && $bandera==0) {
-									echo "<option selected='selected' value='$ciudad' clave='$ciudad'>$ciudad</option>";
-									$banderaS=true;
+
+								if ($ciudad == "Culiacán") {
 								}else{
 									echo "<option value='$ciudad' clave='$ciudad'>$ciudad</option>";
 								}
 							}
+						}
+						else{
+							echo "<select name='opcionCiudades' id='opcionCiudades'>
+							<option value=''>Selecciona un ciudad</option>";
+
+							while ($datos=pg_fetch_array($resultSucursales)) {
+								$ciudad = $datos['ciudad'];
+
+								if ($ciudad == "Culiacán") {
+									echo "<option value='$ciudad' clave='$ciudad'>$ciudad</option>";
+								}else{
+									echo "<option value='$ciudad' clave='$ciudad'>$ciudad</option>";
+								}
+							}
+						}
+							
 						?>
+						}
 					</select>
 					</span>
 				</div>
@@ -49,26 +77,10 @@
 			<div class="selectBlanco" style="background: #fff; border: solid 0px #e1e3e6; border-radius: 3px;">
 				<div class="selectorr" id="selectorrsucursal" style="width: 322px; background: url('../img/vistaCliente/icon-select-form-gris.png?embed') no-repeat 100% 0; -webkit-text-overflow: ellipsis;-moz-text-overflow: ellipsis;-ms-text-overflow: ellipsis; -o-text-overflow: ellipsis; text-overflow: ellipsis; display: block; overflow: hidden; white-space: nowrap; background-position: right 0; height: 35px; line-height: 35px; padding-right: 30px; cursor: pointer; width: 99.9% !important; display: block; color: #868788;">
 					<span style="width: 282px;">
-					<select name="Sucursales" id="Sucursales">
+						<?php $contadorT=0;?>
+					<select name="Sucursales" id="Sucursales" onchange="location = this.value;">
 						<option>Selecciona un cine</option>
-						<?php
-						$x=0;
-						if ($banderaS) {
-							while ($datos2=pg_fetch_array($resultSucursales2)) {
-								$id_sucursal = $datos2['id_sucursal'];
-								$nombreS = $datos2['nombre'];
-								$ciudadS = $datos2['ciudad'];
-
-								if($x=0){
-									echo "<option value='$id_sucursal' clave='$id_sucursal'>$nombreS</option>";
-									$x++;
-									$banderaS=false;
-								}else{
-									echo "<option value='$id_sucursal' clave='$id_sucursal'>$nombreS</option>";
-								}
-							}
-						}
-						?>
+						<?php//echo "<option value='cartelera.php?ciudad=$ciudadS3&id_sucursal3=$id_sucursal'>$nombreS3</option>";?>
 						
 					</select>
 					</span>
@@ -76,7 +88,9 @@
 			</div>
 			<a href="#" class="linkTuCine" style="display:block"><span>¿No encuentras tu cine?</span></a>
 		</div>
-		<div class="col2" style="text-decoration: none;"><a href="cartelera.php" class="btn btnEnviar btnVerCartelera" style="text-decoration: none;">VER CARTELERA</a>
+		<?php 
+		$Pciudad = 'Culiacán';
+		echo "<div class='col2' style='text-decoration: none;'><a href='cartelera.php?ciudad=$Pciudad' class='btn btnEnviar btnVerCartelera' style='text-decoration: none;'>VER CARTELERA</a>"; ?>
 		</div>
 	</div>
 </div>
