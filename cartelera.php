b4adddd8b14213e8a3b3e7b4c00381e8e7dda1ec
keniarios <judith23_rios@hotmail.com>
2018-11-23 10:16:16 -0700

    ASDASD

--- cartelera.php
+++ cartelera.php
@@ -1,4 +1,13 @@
-
+<?php 
+	session_start();
+	if(!isset($_SESSION['id_cinepolisid'])) 
+	{
+		$id_cinepolisid = -1;
+	}
+	else{
+		$id_cinepolisid = $_SESSION['id_cinepolisid'];
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
