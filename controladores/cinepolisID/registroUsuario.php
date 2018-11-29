6c6e21d162e7f62df13387e710f7e834eec48c35
keniarios <judith23_rios@hotmail.com>
2018-11-29 08:54:09 -0700

    hgjh

--- controladores/cinepolisID/registroUsuario.php
+++ controladores/cinepolisID/registroUsuario.php
@@ -2,6 +2,7 @@
 
 require_once ('../../bd/conexion.php'); $conexion = conectarBD();
 
+if (isset($_POST['politica'])) {
 	$nombre = mb_convert_case($_POST['txtNombre'], MB_CASE_TITLE, "UTF-8");
 	$appaterno = mb_convert_case($_POST['txtApellidoPaterno'], MB_CASE_TITLE, "UTF-8");
 	$apmaterno = mb_convert_case($_POST['txtApellidoMaterno'], MB_CASE_TITLE, "UTF-8");
@@ -20,8 +21,17 @@ require_once ('../../bd/conexion.php'); $conexion = conectarBD();
 
 	$query = "INSERT INTO registrocinepolisid (nombre, apellidopaterno, apellidomaterno, correo, contrasena, tarjetaclub, preguntaseguridad, respuestapreguntaseguridad, dianacimiento, mesnacimiento, anonacimiento, lada, telefono) VALUES ('$nombre', '$appaterno', '$apmaterno','$correo','$contrasena','$txtTCC','$ddlPreguntaSecreta','$txtRespuestaPregSecret', '$ddlDia', '$ddlMes', '$ddlAnio', '$txtLada', '$txtTelefono')";
 	$result = pg_query($query); 
+}//fin validacion_chkTerms
+else{
+	echo "
+	<script languaje='javascript'>
+	    alert('Debe de Aceptar La Política de Privacidad Cinépolis.');
+	    location.href = '../haztupago.php';
+	</script>
+	";
+}
 ?>
 	<script languaje="javascript">
-	    alert('Se registro correctamente, revise su correo para la confirmación.');
+	    alert('Se registro correctamente.');
 	    location.href = "../../index.php";
 	</script>
