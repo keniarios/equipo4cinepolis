2da9aa41f967e9b96135ecf3abdbda1482929596
keniarios <judith23_rios@hotmail.com>
2018-12-03 08:51:35 -0700

    dasdasd

--- controladores/finalizar-Compra.php
+++ controladores/finalizar-Compra.php
@@ -33,7 +33,7 @@
 
 
 	$query = "INSERT INTO ventas (id_horario, id_tarjeta , id_usuario, asientos_seleccionados, cantidadboletos3raedad, cantidadboletosadultos, cantidadboletosninos, precioboletos3raedad, precioboletosadultos, precioboletosninos, horacompra, fechacompra, pagototal) 
-	VALUES ('$id_horario', '$id_tarjeta', '$id_cinepolisid', '$asientos_seleccionados', '$Cedad3era', '$Cadulto', '$Cninos', '$precioboletos3raedad', '$precioboletosadultos', '$precioboletosninos', '$horaActual', '$fechaActual', '$PrecioTotal')";
+	VALUES ('$id_horario', '$id_tarjeta', '$id_cinepolisid', '$asientos_seleccionados', '$Cedad3era', '$Cadulto', '$Cninos', '$precioTotal3raEdad', '$precioTotalAdulto', '$precioTotalNino', '$horaActual', '$fechaActual', '$PrecioTotal')";
 	pg_query($query);
 
 	$_SESSION['id_horario'] = 0;
