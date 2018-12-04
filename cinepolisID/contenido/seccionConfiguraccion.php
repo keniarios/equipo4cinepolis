d7468461416d95171b34284c065028178a185b90
keniarios <judith23_rios@hotmail.com>
2018-12-04 10:33:55 -0700

    dasdas

--- cinepolisID/contenido/seccionConfiguraccion.php
+++ cinepolisID/contenido/seccionConfiguraccion.php
@@ -191,43 +191,42 @@
                     <strong for="">Fecha de nacimiento</strong>
                     <div class="row">
                         <div class="id-col4">
-                            <div class="" id="uniform-ddlDia" style="width: 87px;">
+                            
                             <select name="ddlDia" id="ddlDia" class="ddl-id">
-	<option value="">Día</option>
-	<option value="01">1</option>
-	<option value="02">2</option>
-	<option value="03">3</option>
-	<option value="04">4</option>
-	<option value="05">5</option>
-	<option value="06">6</option>
-	<option value="07">7</option>
-	<option value="08">8</option>
-	<option value="09">9</option>
-	<option value="10">10</option>
-	<option value="11">11</option>
-	<option value="12">12</option>
-	<option value="13">13</option>
-	<option value="14">14</option>
-	<option value="15">15</option>
-	<option value="16">16</option>
-	<option value="17">17</option>
-	<option value="18">18</option>
-	<option value="19">19</option>
-	<option value="20">20</option>
-	<option value="21">21</option>
-	<option value="22">22</option>
-	<option value="23">23</option>
-	<option value="24">24</option>
-	<option value="25">25</option>
-	<option value="26">26</option>
-	<option value="27">27</option>
-	<option value="28">28</option>
-	<option value="29">29</option>
-	<option value="30">30</option>
-	<option value="31">31</option>
+                            	<option value="">Día</option>
+                            	<option value="01">1</option>
+                            	<option value="02">2</option>
+                            	<option value="03">3</option>
+                            	<option value="04">4</option>
+                            	<option value="05">5</option>
+                            	<option value="06">6</option>
+                            	<option value="07">7</option>
+                            	<option value="08">8</option>
+                            	<option value="09">9</option>
+                            	<option value="10">10</option>
+                            	<option value="11">11</option>
+                            	<option value="12">12</option>
+                            	<option value="13">13</option>
+                            	<option value="14">14</option>
+                            	<option value="15">15</option>
+                            	<option value="16">16</option>
+                            	<option value="17">17</option>
+                            	<option value="18">18</option>
+                            	<option value="19">19</option>
+                            	<option value="20">20</option>
+                            	<option value="21">21</option>
+                            	<option value="22">22</option>
+                            	<option value="23">23</option>
+                            	<option value="24">24</option>
+                            	<option value="25">25</option>
+                            	<option value="26">26</option>
+                            	<option value="27">27</option>
+                            	<option value="28">28</option>
+                            	<option value="29">29</option>
+                            	<option value="30">30</option>
+                            	<option value="31">31</option>
 
-</select></div>
-                            <span id="ContentPlaceHolder1_sitio_rfvDia" class="validacion" style="display:none;"></span>
+                            </select>
                         </div>
                         <div class="id-col4">
                             <div class="" id="uniform-ddlMes" style="width: 138px;">
